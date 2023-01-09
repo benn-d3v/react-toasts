83a4b0e2936225933025bb378d46378de3d7ff88
Wrldhorses <wrldhorses@gmail.com>
2023-01-09 13:22:12 +0000

    whitespace fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,11 @@ export function Home() {
 
 	const [addErrorToast, addSuccessToast] = useToastContext();
 
-  return (
-    <>
+	return (
+		<>
 			<h1>Simple React Toasts</h1>
 			<button onClick={() => { addSuccessToast('Product X added to cart.') }}>Success</button>
 			<button onClick={() => { addErrorToast('Payment failed due to X error.') }}>Error</button>
-    </>
-  )
+		</>
+	)
 }
